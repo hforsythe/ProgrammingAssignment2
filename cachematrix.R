c5202df3929473bdb75eafe16a505a45ed765a7b
hforsythe <hforsythe@bellsouth.net>
2015-01-24 17:35:23 -0600

    upload assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,18 @@
 ## For this assignment, we assume that the matrix supplied is always invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(solve) m <<- solve
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
-
 
 ## cacheSolve: This function computes the inverse of the special "matrix" 
 ## returned by makeCacheMatrix above. 
@@ -18,4 +27,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()  # Test if inverse has been calculated
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
 }
+
+
